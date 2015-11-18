98b6201f5bf26f5608bc57d7d5aad088d02a94a4
kcmallard <kylemallard@gmail.com>
2015-11-17 21:09:08 -0600

    Merge Profiles Page into About Page

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
-  get 'pages/profiles'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
